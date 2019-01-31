32e44a121d08ab3b1bf31d30211f09a1b6378c0c
Andrey Molchanovskiy <a.molchanovskiy@gmail.com>
2019-01-31 15:48:08 +0200

    added border to photo views

--- JSQMessagesViewController/Factories/JSQMessagesMediaViewBubbleImageMasker.m
+++ JSQMessagesViewController/Factories/JSQMessagesMediaViewBubbleImageMasker.m
@@ -46,13 +46,13 @@
 - (void)applyOutgoingBubbleImageMaskToMediaView:(UIView *)mediaView
 {
     JSQMessagesBubbleImage *bubbleImageData = [self.bubbleImageFactory outgoingMessagesBubbleImageWithColor:[UIColor whiteColor]];
-    [self jsq_maskView:mediaView withImage:[bubbleImageData messageBubbleImage]];
+    [self jsq_maskView:mediaView withImage:[bubbleImageData messageBubbleImage] icomming:NO];
 }
 
 - (void)applyIncomingBubbleImageMaskToMediaView:(UIView *)mediaView
 {
     JSQMessagesBubbleImage *bubbleImageData = [self.bubbleImageFactory incomingMessagesBubbleImageWithColor:[UIColor whiteColor]];
-    [self jsq_maskView:mediaView withImage:[bubbleImageData messageBubbleImage]];
+    [self jsq_maskView:mediaView withImage:[bubbleImageData messageBubbleImage] icomming:YES];
 }
 
 + (void)applyBubbleImageMaskToMediaView:(UIView *)mediaView isOutgoing:(BOOL)isOutgoing
@@ -69,7 +69,7 @@
 
 #pragma mark - Private
 
-- (void)jsq_maskView:(UIView *)view withImage:(UIImage *)image
+- (void)jsq_maskView:(UIView *)view withImage:(UIImage *)image icomming:(BOOL)incoming
 {
     NSParameterAssert(view != nil);
     NSParameterAssert(image != nil);
@@ -78,6 +78,30 @@
     imageViewMask.frame = CGRectInset(view.frame, 2.0f, 2.0f);
     
     view.layer.mask = imageViewMask.layer;
+    
+        UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:imageViewMask.bounds
+                                                   byRoundingCorners:UIRectCornerAllCorners
+                                                         cornerRadii:CGSizeMake(16.0f, 16.0f)];
+    
+    CAShapeLayer *maskLayer = [CAShapeLayer layer];
+    maskLayer.frame = imageViewMask.bounds;
+    maskLayer.path = maskPath.CGPath;
+    view.layer.mask = maskLayer;
+    
+    //
+    // And then create the outline layer
+    //
+    CAShapeLayer *shape = [CAShapeLayer layer];
+    shape.frame = imageViewMask.bounds;
+    shape.path = maskPath.CGPath;
+    shape.lineWidth = 2.0f;
+    if (incoming) {
+        shape.strokeColor = [UIColor colorWithRed:0.231 green:0.631 blue:0.898 alpha:1.000].CGColor; // set color as you want.;
+    } else {
+        shape.strokeColor = [UIColor colorWithRed:0.914 green:0.486 blue:0.082 alpha:1.000].CGColor;
+    }
+    shape.fillColor = [UIColor clearColor].CGColor;
+    [view.layer addSublayer:shape];
 }
 
 @end
